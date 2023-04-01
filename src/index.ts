7f297fa80fa0110ee8bde7168480b2bc49741233
nkowne63rt <neutron63zf@gmail.com>
2023-04-02 02:00:02 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+(await import("dotenv")).config();
+import { OpenAI } from "langchain";
+import { initializeAgentExecutor, AgentExecutor } from "langchain/agents";
+import { SerpAPI, Calculator } from "langchain/tools";
+
+const model = new OpenAI({ temperature: 0 });
+const tools = [new SerpAPI(), new Calculator()];
+
+const executor = await initializeAgentExecutor(
+  tools,
+  model,
+  "zero-shot-react-description"
+);
+console.log("Loaded agent.");
+
+AgentExecutor.fromAgentAndTools({
+  agent: executor.agent,
+  tools,
+});
+
+const input =
+  "Who is Olivia Wilde's boyfriend?" +
+  " What is his current age raised to the 0.23 power?";
+console.log(`Executing with input "${input}"...`);
+
+const result = await executor.call({ input });
+
+console.log(`Got output ${result.output}`);
